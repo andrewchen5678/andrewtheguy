61b9196b01a5d75a55a2841830155b62ed3f9742
Andrew Chen <andrew@xtopoly.com>
2015-08-25 18:01:34 -0700

    fix canvas

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,5 @@
 //= // require jquery/dist/jquery.js
 //= require bootstrap-sass/assets/javascripts/bootstrap-sprockets.js
-//= require angular/angular.js
+//= //require angular/angular.js
 //= require includes/global.js
 //= require_tree ./includes/
