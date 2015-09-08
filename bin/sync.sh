dca405178da23cc48f0e088e92458e2411168887
Andrew Chen <andrew@xtopoly.com>
2015-09-07 22:08:25 -0700

    sync script fix

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 rsync -rvzu -e 'ssh -p 2244' build/ andrewtheguy@server.techywolf.com:/var/www/andrewtheguy/
+ssh -p 2244 andrewtheguy@server.techywolf.com 'find /var/www/andrewtheguy/ -type f -exec chmod 0644 {} +'
